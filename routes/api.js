38b89f58c93eb105c9b5202bec7bb39e32e0453f
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-08-15 12:31:47 +0200

    add coding for /api/whoami

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  });
+});
+
 router.get('/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
